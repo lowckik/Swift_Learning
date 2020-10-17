a3b87a88a6a275dfb2d40d459083b5ea2e163931
Dmytriy_LW <lowckik@icloud.com>
2020-10-17 18:19:58 +0300

    HW_1

---
+++ HW_1Kovriga_Dmytriy.playground/Pages/2_CW_2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,40 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//let x = 5
+//var y = x + 3
+//y += 2
+//let pincodeCount = 0
+//let isLoggedIn = pincodeCount != 0
+//let firstLaunch = true
+//
+//firstLaunch ? print(1) : print(2)
+//
+//let a = firstLaunch ? 1 : 2
+
+//---------------------------------------------
+//let nextStep = "mail"
+//let isLoggedIn = false
+//
+//
+//switch nextStep {
+//case "mail" where isLoggedIn : print("экран регистрации")
+//case "mail" where !isLoggedIn : print("экран регистрации")
+//case "phone" : print("код смс")
+//case "code" : print("аутентификатор")
+//default : fatalError()
+//}
+//
+//// where - указываем что там где
+//// fallthrough - заставляет помимо кейса в который мы попали выполнить еще и следующий за ним в любом случае
+////switch nextStep {
+////case "mail" where isLoggedIn :
+////    print("экран регистрации")
+////    fallthrough
+////case "phone" : print("код смс")
+////default : fatalError()
+////}
+//---------------------------------------------
+
+//: [Next](@next)
